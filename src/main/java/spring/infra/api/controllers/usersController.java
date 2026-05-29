cd2e6f1943def577256b5422d3161271f77bb9af
Diego Sousa <devdiegosousa@gmail.com>
2026-05-29 00:54:09 +0000

    feat: all database created

--- src/main/java/spring/infra/api/controllers/usersController.java
+++ src/main/java/spring/infra/api/controllers/usersController.java
@@ -1,21 +1,22 @@
-package spring.infra.api.controllers;
+// package spring.infra.api.controllers;
 
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+// import jakarta.validation.Valid;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("api/v1/auth")
-public class usersController {
+// @RestController
+// @RequestMapping("api/v1/auth")
+// public class usersController {
 
-    @Autowired
-    authService authService;
+//     @Autowired
+//     authService authService;
 
-    @GetMapping("/signup")
-    public ResponseEntity<?> signup(@Valid @RequestBody SignupRequestDTO signupRequest){
-        try{
-            auth
-        }
-    }
-}
+//     @GetMapping("/signup")
+//     public ResponseEntity<?> signup(@Valid @RequestBody SignupRequestDTO signupRequest){
+//         try{
+//         } catch(exception e) {
+//             return ResponseEntity.status(500);
+//         };
+//     }
+// }
--- src/main/java/spring/infra/api/services/AuthService.java
+++ src/main/java/spring/infra/api/services/AuthService.java
@@ -1,12 +1,11 @@
-package spring.infra.api.services;
+// package spring.infra.api.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
 
-@Service
-public class AuthService {
-    @Autowired
-    private userRepository userRepository;
-    @Autowired
-
-}
+// @Service
+// public class AuthService {
+//     @Autowired
+//     private userRepository userRepository;
+// }
+//
